fa03efb640b4fa0680598d4488d253884f7fde6a
Abhinav Anand <abhinavanand4987@gmail.com>
2021-02-20 04:44:17 +0530

    20-2-2021 || 6Q

---
+++ Binary Trees/rightView.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *left, *right;
+};
+
+node *get_newNode(int input)
+{
+    node *nya = new node();
+    nya->data = input;
+    nya->left = NULL;
+    nya->right = NULL;
+
+    return nya;
+}
+
+node *addNode(node *parent, int input)
+{
+
+    if (parent == NULL)
+    {
+        parent = get_newNode(input);
+    }
+    else if (parent->data == input)
+    {
+        cout << "already present in the tree ";
+    }
+    else if (parent->data > input)
+    {
+        parent->left = addNode(parent->left, input);
+    }
+    else
+    {
+        parent->right = addNode(parent->right, input);
+    }
+
+    return parent;
+}
+
+int get_replacingNode(node *vertex)
+{
+    if (vertex->left == NULL)
+    {
+        return vertex->data;
+    }
+    else
+    {
+        return get_replacingNode(vertex->left);
+    }
+}
+
+node *deleteNode(int index, node *vertex)
+{
+    if (vertex == NULL)
+    {
+        cout << " requested node not found!! :)";
+    }
+    else if (index > vertex->data)
+    {
+        vertex->right = deleteNode(index, vertex->right);
+    }
+    else if (index < vertex->data)
+    {
+        vertex->left = deleteNode(index, vertex->left);
+    }
+    else
+    {
+        if (vertex->right == NULL)
+        {
+            node *returNode = vertex->right;
+            delete vertex;
+            return returNode;
+        }
+        else
+        {
+            int replace = get_replacingNode(vertex->right);
+            vertex->data = replace;
+            vertex->right = deleteNode(replace, vertex->right);
+            return vertex;
+        }
+    }
+    return vertex;
+}
+
+void rightView(node *vertex)
+{
+    if (vertex == NULL)
+    {
+        return;
+    }
+    else
+    {
+        cout << vertex->data << " ";
+        if (vertex->right != NULL)
+        {
+            rightView(vertex->right);
+        }
+        else
+        {
+            rightView(vertex->left);
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    node *root = new node();
+    root->left = NULL;
+    root->right = NULL;
+
+    for (int i = 0; i < n; i++)
+    {
+
+        int input;
+        cin >> input;
+
+        if (i == 0)
+        {
+            root->data = input;
+        }
+        else
+        {
+            if (root->data > input)
+            {
+                root->left = addNode(root->left, input);
+            }
+            else
+            {
+                root->right = addNode(root->right, input);
+            }
+        }
+    }
+
+    rightView(root);
+}
+
+// 67 34 89 23 45 79 90 15 70 85
